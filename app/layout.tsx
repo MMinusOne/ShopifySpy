077ac75bce3d714e5904a03b54adeaca683819d9
MMinusOne <michael.business.lb@proton.me>
2024-10-11 07:43:54 +0300

    Adsterra removal

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,8 @@
 "use client";
 
-import AdsterraAd from "@/components/lib/AdsTerra";
 import "@/styles/globals.scss";
 
 import { ClerkProvider } from "@clerk/nextjs";
-import type { Metadata } from "next";
 
 export default function RootLayout({
   children,
@@ -14,17 +12,9 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" data-theme="emerald" className="w-full h-full">
+      <script type='text/javascript' src='//pl24659944.cpmrevenuegate.com/0e/8d/96/0e8d962c7cfe8f9df33d174022462121.js'></script>
         <body className="w-full">
           {children}
-          <AdsterraAd
-            atOptions={{
-              key: 'b7081c03a15d9a99b3866fcb2327a6e4',
-              format: 'iframe',
-              height: 60,
-              width: 468,
-              params: {}
-            }}
-          />
         </body>
       </html>
     </ClerkProvider>
